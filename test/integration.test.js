cfe2803025e3323dff0b2ea09aff42cf3166e736
Ahmad Amireh <ahmad@amireh.net>
2021-06-30 14:56:18 -0600

    migrate to dart-sass away from node-sass
    node-sass has been deprecated for a while and it'd be nice to get rid of
    the node-gyp dependency in canvas builds

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const fsX = require('fs-extra-promise')
 const { assert } = require('chai')
+const css = require('css')
 
 describe('brandable_css', () => {
   const dist = path.resolve(__dirname, '../tmp/dist')
@@ -28,30 +29,30 @@ describe('brandable_css', () => {
     )
 
     assert.deepEqual(manifest, {
+      "a.scss$$$$$$$$$$$normal_contrast": {
+        "combinedChecksum": "542ecd8ef1",
+        "includedFiles": [
+          "a.scss"
+        ],
+        "includesNoVariables": true
+      },
       "a.scss$$$$$$$$$$$high_contrast": {
-        "combinedChecksum": "847ced33a4",
+        "combinedChecksum": "542ecd8ef1",
         "includedFiles": [
           "a.scss"
         ],
         "includesNoVariables": true
       },
       "b.scss$$$$$$$$$$$normal_contrast": {
-        "combinedChecksum": "40d5ecf9cb",
+        "combinedChecksum": "c9f94e5d5e",
         "includedFiles": [
           "b.scss",
           "variants/normal_contrast/_variant_variables.scss"
         ],
         "includesNoVariables": false
       },
-      "a.scss$$$$$$$$$$$normal_contrast": {
-        "combinedChecksum": "847ced33a4",
-        "includedFiles": [
-          "a.scss"
-        ],
-        "includesNoVariables": true
-      },
       "b.scss$$$$$$$$$$$high_contrast": {
-        "combinedChecksum": "fff8a69e40",
+        "combinedChecksum": "266ca318ab",
         "includedFiles": [
           "b.scss",
           "variants/high_contrast/_variant_variables.scss"
@@ -62,11 +63,30 @@ describe('brandable_css', () => {
   })
 
   it('generates variant stylesheets for files that use variables', () => {
-    assert.ok(fsX.existsSync(path.resolve(dist, 'high_contrast/b-fff8a69e40.css')))
-    assert.ok(fsX.existsSync(path.resolve(dist, 'normal_contrast/b-40d5ecf9cb.css')))
+    assert.ok(fsX.existsSync(path.resolve(dist, 'high_contrast/b-266ca318ab.css')))
+    assert.ok(fsX.existsSync(path.resolve(dist, 'normal_contrast/b-c9f94e5d5e.css')))
   })
 
   it('variants as "no_variables" for files that use no variables', () => {
-    assert.ok(fsX.existsSync(path.resolve(dist, 'no_variables/a-847ced33a4.css')))
+    assert.ok(fsX.existsSync(path.resolve(dist, 'no_variables/a-542ecd8ef1.css')))
+  })
+
+  it('applies variant variables', () => {
+    const samples = [
+      { file: 'high_contrast/b-266ca318ab.css', color: 'maroon' },
+      { file: 'normal_contrast/b-c9f94e5d5e.css', color: 'red' },
+    ]
+
+    for (const sample of samples) {
+      const file = fsX.readFileSync(path.resolve(dist, sample.file), 'utf8')
+      const {stylesheet} = css.parse(file)
+
+      assert.deepEqual(stylesheet.rules[0].selectors, ['body'])
+      assert.deepEqual(stylesheet.rules[0].declarations.length, 1)
+      assert.include(stylesheet.rules[0].declarations[0], {
+        property: 'background',
+        value: sample.color
+      })
+    }
   })
 })
